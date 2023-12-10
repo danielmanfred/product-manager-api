b464005d19941cc3b78b00b01eb231e48addc6e8
Daniel Brito <danielmanfred@gmail.com>
2023-12-10 19:24:46 -0300

    build: setup migrations

---
+++ prisma/migrations/20231210221312_/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `description` TEXT NULL,
+    `price` DOUBLE NOT NULL,
+    `idCategory` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
