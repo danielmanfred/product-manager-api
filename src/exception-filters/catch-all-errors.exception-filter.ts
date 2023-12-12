47d8e227f03bb809708751f91668e51b34f0b3e9
Daniel Brito <danielmanfred@gmail.com>
2023-12-12 15:22:58 -0300

    build: use repl and swc for debug and speed

--- src/exception-filters/catch-all-errors.exception-filter.ts
+++
@@ -1,9 +0,0 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpServer } from "@nestjs/common";
-import { AbstractHttpAdapter, BaseExceptionFilter, HttpAdapterHost } from "@nestjs/core";
-
-@Catch()
-export class CatchAllErrorsExceptionFilter extends BaseExceptionFilter {
-    catch(exception: any, host: ArgumentsHost): void {
-        return super.catch(exception, host);
-    }
-}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './exception-filters/prisma.exception-filter';
-import { CatchAllErrorsExceptionFilter } from './exception-filters/catch-all-errors.exception-filter';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(
-    new CatchAllErrorsExceptionFilter(),
     new PrismaExceptionFilter()
   )
 
